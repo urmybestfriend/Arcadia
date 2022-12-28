f6cd7a2ce5093252293bf1c89fa1a3b247ab398a
urmybestfriend <c.goldentiger.c@gmail.com>
2022-12-27 20:02:43 -0500

    Updated

--- src/container/Content/index.js
+++ src/container/Content/index.js
@@ -6,7 +6,7 @@ export default function Content() {
             <div className="grid xxl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 grid-cols-1">
                 <div className="apply-field">
                     <div>
-                        <h1 className="arcadia-text">ARCADIA</h1>
+                        <span className="arcadia-text">ARCADIA</span>
                         <h3 className="accelerator-text">WEB3 ACCELERATOR</h3>
                         <p>Premier Web3 Accelerator, providing creators and communities with the tools and services to create a better tomorrow</p>
                         <button type="button" id="apply-button">APPLY NOW</button>
